ac095095542e94bf002eb4f6a62b6beec1e72b1c
Prabhat-Thapa45 <akoup66@gmail.com>
2025-08-26 12:24:06 +0545

    remove db files

--- src/app/db/index.ts
+++
@@ -1,8 +0,0 @@
-import * as schema from './schema';
-import drizzle from 'drizzle-orm/postgres-js';
-
-
-const db = drizzle({schema});
-
-await db.query.reviewsTable.findMany();
-export default Response(db);
